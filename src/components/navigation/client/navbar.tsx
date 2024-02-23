bd546adcaba83c20f7dfca6f36feae9ea434c675
Paula Domingues <pdomingues@itba.edu.ar>
2024-02-22 22:37:47 -0300

    fix: more fixes to deploy on vercel

--- src/components/navigation/client/navbar.tsx
+++ src/components/navigation/client/navbar.tsx
@@ -16,13 +16,13 @@ export default function Navbar() {
                 className="block h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 aria-hidden="true"
               >
                 <path
-                  stroke-linecap="round"
-                  // stroke-linejoin="round"
+                  strokeLinecap="round"
+                  // strokeLinejoin="round"
                   d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                 />
               </svg>
@@ -30,13 +30,13 @@ export default function Navbar() {
                 className="hidden h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 aria-hidden="true"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M6 18L18 6M6 6l12 12"
                 />
               </svg>
