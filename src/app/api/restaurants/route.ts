cf674fcab96ee418d100dcd293a4a76bedf23cf7
Franco Rodriguez <64867075+robrodriguez99@users.noreply.github.com>
2024-02-24 16:15:15 -0300

    fix: nextjs 14 is trolling me

--- src/app/api/restaurants/route.ts
+++ src/app/api/restaurants/route.ts
@@ -1,26 +1,26 @@
 
 import { RestaurantController } from '@/controllers/restaurant.controller';
 
-export default async function handler(request: Request) {
+
+export async function POST(request: Request) {
   const restaurantController: RestaurantController = RestaurantController.getInstance() //TODO: dont use getInstance?
-  const { method } = request
-  if (method === 'GET') {
-    const result = await restaurantController.getRestaurants()
-    return result
 
-  } else if (method === 'POST') {
-    const { name, email, password } = await request.json()
-    const result = await restaurantController.createRestaurant(name, email, password)
-    return result
+  const { email, password } = await request.json()
+  const result = await restaurantController.login(email, password)
+  return result
+}
 
-  } else if (method === 'DELETE') {
-    const { id } = await request.json()
-    const result = await restaurantController.retireRestaurant(id)
-    return result
+export async function GET() {
+  const restaurantController: RestaurantController = RestaurantController.getInstance() //TODO: dont use getInstance()
+  const result = await restaurantController.getRestaurants()
+  return result
+}
 
-  } else {
-    return new Response('Method not allowed', { status: 405 })
-  }
+export async function DELETE(request: Request) {
+  const restaurantController: RestaurantController = RestaurantController.getInstance() //TODO: dont use getInstance()
+  const { id } = await request.json()
+  const result = await restaurantController.retireRestaurant(id)
+  return result
 }
 
 
