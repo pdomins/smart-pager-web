bdef1d50564520d3ed5a1c45a422e31c1fcaecf0
Franco Rodriguez <64867075+robrodriguez99@users.noreply.github.com>
2024-02-26 12:49:20 -0300

    fix: removed controller from routes

--- src/app/api/restaurants/route.ts
+++ src/app/api/restaurants/route.ts
@@ -1,26 +1,25 @@
+// TODO: remove this file later on. This is just for reference
 
-import { RestaurantController } from '@/controllers/restaurant.controller';
+// import RestaurantService from "@/services/restaurant.service"
 
+// export async function POST(request: Request) {
 
-export async function POST(request: Request) {
-  const restaurantController: RestaurantController = RestaurantController.getInstance() //TODO: dont use getInstance?
+//   const { email, password } = await request.json()
+//   const result = await RestaurantService.getInstance.login(email, password)
+//   return result
+// }
 
-  const { email, password } = await request.json()
-  const result = await restaurantController.login(email, password)
-  return result
-}
+// export async function GET() {
+//   const restaurantController: RestaurantController = RestaurantController.getInstance() //TODO: dont use getInstance()
+//   const result = await restaurantController.getRestaurants()
+//   return result
+// }
 
-export async function GET() {
-  const restaurantController: RestaurantController = RestaurantController.getInstance() //TODO: dont use getInstance()
-  const result = await restaurantController.getRestaurants()
-  return result
-}
-
-export async function DELETE(request: Request) {
-  const restaurantController: RestaurantController = RestaurantController.getInstance() //TODO: dont use getInstance()
-  const { id } = await request.json()
-  const result = await restaurantController.retireRestaurant(id)
-  return result
-}
+// export async function DELETE(request: Request) {
+//   const restaurantController: RestaurantController = RestaurantController.getInstance() //TODO: dont use getInstance()
+//   const { id } = await request.json()
+//   const result = await restaurantController.retireRestaurant(id)
+//   return result
+// }
 
 
