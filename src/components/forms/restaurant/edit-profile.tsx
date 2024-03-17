61be0ab6a7059a46a7b7aa5b6e8686ffd39cd5d2
Franco Rodriguez <64867075+robrodriguez99@users.noreply.github.com>
2024-03-17 16:35:25 -0300

    feat: confirmation modal

--- src/components/forms/restaurant/edit-profile.tsx
+++ src/components/forms/restaurant/edit-profile.tsx
@@ -1,3 +1,4 @@
+import Modal from "@/components/utils/confirmation-modal";
 import DefaultInput from "@/components/utils/inputs/default-input";
 import { Restaurant } from "@/types/restaurant";
 import { useState } from "react";
@@ -6,10 +7,22 @@ export default function EditRestaurantProfile ({restaurantData, toggleProfileFor
     const [name, setName] = useState(restaurantData.name);
     const [email, setEmail] = useState(restaurantData.email);
     const [restaurantName, setRestaurantName] = useState(restaurantData.name); //TODO: Change to restaurantName
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [modalMesaage, setModalMessage] = useState('Are you sure?');
 
+    const toggleModalVisibility = () => {
+        setIsModalVisible(!isModalVisible);
+    }
+
+    const handleAccept = () => {
+        console.log('handleAccept'); //TODO: Implement handleAccept
+        toggleModalVisibility();
+        toggleProfileFormVisibility();
+    }
 
     return (
         <div className="my-2 w-full bg-white overflow-hidden rounded-lg shadow-md transition duration-300 ease-in-out  relative">
+            { isModalVisible && <Modal message={modalMesaage} onAccept={handleAccept} onCancel={toggleModalVisibility}  />}
             <div className="m-4">
                 <div className="m-3 flex justify-center">
                     <DefaultInput label="Nombre" name="name" value={name} onChange={setName} />
@@ -22,9 +35,16 @@ export default function EditRestaurantProfile ({restaurantData, toggleProfileFor
                 </div>
             </div>
             <div className="flex justify-evenly m-3 text-xl">
-                <button className="text-red-500 hover:text-red-800" onClick={toggleProfileFormVisibility}>Cancelar</button>
-                <button className="text-green-500 ml-3 hover:text-green-800" onClick={toggleProfileFormVisibility}>Guardar</button>
+                <button className="text-red-500 hover:text-red-800" onClick={() =>{
+                    setModalMessage('Si cancelas perderás los cambios realizados. ¿Estás seguro que quieres cancelar?')
+                    setIsModalVisible(true)
+                } }>Cancelar</button>
+                <button className="text-green-500 ml-3 hover:text-green-800" onClick={() =>{
+                    setModalMessage('¿Estás seguro que quieres guardar los cambios?')
+                    setIsModalVisible(true)
+                }}>Guardar</button>
             </div>
         </div>
     )
 }
+
