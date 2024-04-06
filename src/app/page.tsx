814793f78c9e8fc5d391ec9be306a6f2d1f5d205
Paula Domingues <pdomingues@itba.edu.ar>
2024-04-06 13:49:01 -0300

    fix: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 'use client'
 
+import Navbar from '@/components/navigation/restaurants/navbar'
 import RestaurantLanding from '@/components/restaurants/restaurant-landing'
 
 export default function Page() {
-  return <RestaurantLanding />
+  return (
+    <>
+      <Navbar />
+      <RestaurantLanding />
+    </>
+  )
 }
--- src/components/navigation/restaurants/navbar.tsx
+++ src/components/navigation/restaurants/navbar.tsx
@@ -1,91 +1,137 @@
 'use client'
-import { signIn, signOut, useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import Hand from 'public/hand.png'
+
+import Container from '@/components/restaurants/new/container'
+import { useSession } from 'next-auth/react'
+// import { useRouter } from 'next/navigation'
 export default function Navbar() {
   const { data: session } = useSession()
-  const router = useRouter()
+  // const router = useRouter()
+
+  const unsignedlinks = [
+    {
+      to: '/#',
+      label: 'Registrarse',
+    },
+    {
+      to: '/#',
+      label: 'Iniciar Sesión',
+    },
+  ]
+
+  const signedLinks = [
+    {
+      to: '/#',
+      label: 'Mi Restaurante',
+    },
+    {
+      to: '/#',
+      label: 'Lista de Comensales',
+    },
+    {
+      to: '/#',
+      label: 'Lista de Pedidos',
+    },
+    {
+      to: '/#',
+      label: 'Cerrar sesión',
+    },
+  ]
+
+  const links = session ? signedLinks : unsignedlinks
+
   return (
-    <nav>
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden" />
-          <div className="flex flex-1 items-emnn justify-end sm:items-stretch sm:justify-end">
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                {session && (
-                  <div className="flex space-x-16">
-                    <button
-                      // onClick={() =>  signIn()}
-                      onClick={() => router.push('restaurant')}
-                      className="font-averta font-light text-custom-blue hover:text-indigo-500 px-3 py-2 font-lg"
-                      aria-current="page"
+    <nav className="absolute z-10 w-full border-b border-black/5 lg:border-transparent">
+      <Container>
+        <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 md:gap-0 md:py-4">
+          <div className="relative z-20 flex w-full justify-between md:px-0 lg:w-max">
+            <div className="flex items-center content-center">
+              <Image
+                src={Hand.src}
+                alt="Logo"
+                width={80}
+                height={80}
+                unoptimized={true}
+              />
+              <span className="text-2xl font-bold text-gray-900">
+                Smart Pager
+              </span>
+            </div>
+
+            <div className="relative flex max-h-10 items-center lg:hidden">
+              <button
+                aria-label="humburger"
+                id="hamburger"
+                className="relative -mr-6 p-6"
+              >
+                <div
+                  aria-hidden="true"
+                  id="line"
+                  className="m-auto h-0.5 w-5 rounded bg-sky-900 transition duration-300"
+                ></div>
+                <div
+                  aria-hidden="true"
+                  id="line2"
+                  className="m-auto mt-2 h-0.5 w-5 rounded bg-sky-900 transition duration-300"
+                ></div>
+              </button>
+            </div>
+          </div>
+          <div
+            id="navLayer"
+            aria-hidden="true"
+            className="fixed inset-0 z-10 h-screen w-screen origin-bottom scale-y-0 bg-white/70 backdrop-blur-2xl transition duration-500 lg:hidden"
+          ></div>
+          <div
+            id="navlinks"
+            className="invisible absolute top-full left-0 z-20 w-full origin-top-right translate-y-1 scale-90 flex-col flex-wrap justify-end gap-6 rounded-3xl border border-gray-100 bg-white p-8 opacity-0 shadow-2xl shadow-gray-600/10 transition-all duration-300 lg:visible lg:relative lg:flex lg:w-7/12 lg:translate-y-0 lg:scale-100 lg:flex-row lg:items-center lg:gap-0 lg:border-none lg:bg-transparent lg:p-0 lg:opacity-100 lg:shadow-none"
+          >
+            <div className="w-full text-gray-600  lg:w-auto lg:pr-4 lg:pt-0">
+              <ul className="flex flex-col gap-6 tracking-wide lg:flex-row lg:gap-0 lg:text-sm">
+                {/* <li>
+                  <a
+                    href="https://tailus.gumroad.com/l/astls-premium"
+                    target="_blank"
+                    className="flex gap-2 font-semibold text-gray-700 transition hover:text-primary md:px-4"
+                    rel="noreferrer"
+                  >
+                    <span>Premium</span>
+                  </a>
+                </li> */}
+                {links.map((link) => (
+                  <li key={link.label}>
+                    <a
+                      href={link.to}
+                      className="hover:text-primary block transition md:px-4"
                     >
-                      Mi Restaurante
-                    </button>
-                  </div>
-                )}
-                {!session ? (
-                  <div className="flex space-x-16">
-                    <button
-                      // onClick={() =>  signIn()}
-                      onClick={() =>
-                        signIn()
-                      }
-                      className="font-averta font-light text-custom-blue hover:text-indigo-500 px-3 py-2 font-lg"
-                      aria-current="page"
-                    >
-                      Ingresar
-                    </button>
-                  </div>
-                ) : (
-                  <div className="flex space-x-16">
-                    <button
-                      onClick={() => signOut()}
-                      className="font-averta font-light text-custom-blue hover:text-indigo-500 px-3 py-2 font-lg"
-                      aria-current="page"
-                    >
-                      Cerrar Sesión
-                    </button>
-                  </div>
-                )}
-              </div>
+                      <span>{link.label}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
-      </div>
-      {/* 
-        //<!-- Mobile menu, show/hide based on menu state. --> 
-        <div className="sm:hidden" id="mobile-menu">
-          <div className="space-y-1 px-2 pb-3 pt-2">
-            // <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> 
-            <a
-              href="#"
-              className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-              aria-current="page"
-            >
-              Dashboard
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Team
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Projects
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Calendar
-            </a>
-          </div>
-        </div> 
-        */}
+      </Container>
     </nav>
   )
+  {
+    /* <style>
+    #toggle_nav:checked ~ div #hamburger #line {
+        @apply translate-y-1.5 rotate-45;
+    }
+
+    #toggle_nav:checked ~ div #hamburger #line2 {
+        @apply -translate-y-1 -rotate-45;
+    }
+
+    .toggled div:first-child {
+        @apply translate-y-1.5 rotate-45;
+    }
+    .toggled div:last-child {
+        @apply -translate-y-1 -rotate-45;
+    }
+</style> */
+  }
 }
