47e3c5f49b7b064bd282e375530fb869e97f5e37
Paula Domingues <pdomingues@itba.edu.ar>
2024-04-08 11:54:09 -0300

    fix: react-ive way to handle commensal queue form

--- src/components/forms/commensalQueueForm.tsx
+++ src/components/forms/commensalQueueForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { FormEvent, useState } from 'react'
 import CloseIcon from '@mui/icons-material/Close'
 import { useParams, useRouter } from 'next/navigation'
 import { addCommensal } from '@/repositories/queue-repository'
@@ -8,56 +8,50 @@ export default function CommensalQueueForm({
 }: {
   toggleCommensalFormVisibility: () => void
 }) {
-  const restaurantSlug = useParams<{ restaurant: string }>()
+  const { restaurant: restaurantSlug } = useParams<{ restaurant: string }>()
   const router = useRouter()
 
-  useEffect(() => {
-    const form = document.getElementById('queueForm')
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [commensals, setCommensals] = useState('1')
+  const [phone, setPhone] = useState('')
+  const [description, setDescription] = useState('')
 
-    const handleQueueFormSubmit = async (e: Event) => {
-      e.preventDefault()
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
 
-      const emailInput = document.getElementById(
-        'form-email'
-      ) as HTMLInputElement | null
-      const nameInput = document.getElementById(
-        'form-name'
-      ) as HTMLInputElement | null
-      const commensalsInput = document.getElementById(
-        'form-commensals'
-      ) as HTMLInputElement | null
-
-      if (emailInput && nameInput && commensalsInput) {
-        const success = await addCommensal({
-          restaurantSlug: restaurantSlug.restaurant,
-          email: emailInput.value,
-          clientData: {
-            name: nameInput.value,
-            groupSize: commensalsInput.value,
-            description: '',
-            phoneNumber: '',
-          },
-        })
-        if (success)
-          router.push(
-            '/restaurants/' + restaurantSlug.restaurant + '/queued/commensal'
-          )
-        else
-          router.push('/restaurants/' + restaurantSlug.restaurant + '/queued')
-      } else {
-        console.error('Some form elements are missing or inaccessible.')
-      }
+    if (!restaurantSlug || typeof restaurantSlug !== 'string') {
+      console.error('Restaurant slug is missing')
+      return
     }
-    if (form) {
-      form.addEventListener('submit', handleQueueFormSubmit)
-    }
+    console.log({
+      restaurantSlug,
+      email,
+      clientData: {
+        name,
+        groupSize: commensals,
+        description,
+        phoneNumber: phone,
+      },
+    })
 
-    return () => {
-      if (form) {
-        form.removeEventListener('submit', handleQueueFormSubmit)
-      }
+    const success = await addCommensal({
+      restaurantSlug,
+      email,
+      clientData: {
+        name,
+        groupSize: commensals,
+        description,
+        phoneNumber: phone,
+      },
+    })
+
+    if (success) {
+      router.push(`/restaurants/${restaurantSlug}/queued/commensal`)
+    } else {
+      console.error('Failed to add commensal')
     }
-  }, [restaurantSlug, router])
+  }
 
   return (
     <div className="min-h-screen flex flex-col justify-center relative">
@@ -72,82 +66,125 @@ export default function CommensalQueueForm({
           Anótese en la cola
         </h1>
         <p className="mt-2 text-gray-700">
-          Deje sus datos y le avisaremos cuando su mesa este preparada
+          Deje sus datos y le avisaremos cuando su mesa esté preparada.
         </p>
       </div>
       <div className="flex justify-center">
-        <form id="queueForm" className="mt-6 w-full max-w-lg">
+        <form className="mt-6 w-full max-w-lg" onSubmit={handleSubmit}>
+          {/* Email */}
+          <InputField
+            label="Email"
+            type="email"
+            placeholder="ejemplo@mail.com"
+            value={email}
+            onChange={setEmail}
+            required={true}
+          />
+
+          {/* Nombre Completo */}
+          <InputField
+            label="Nombre Completo"
+            type="text"
+            placeholder="Su nombre y apellido"
+            value={name}
+            onChange={setName}
+            required={true}
+          />
+
+          {/* Cantidad de comensales */}
           <div className="w-full px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Email <span className="text-red-500">*</span>
-            </label>
-            <input
-              className="appearance-none block w-full bg-white-200 text-gray-700 shadow border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="form-email"
-              type="email"
-              placeholder="ejemplo@mail.com"
-              required
-            />
-          </div>
-          <div className="w-full px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Nombre Completo <span className="text-red-500">*</span>
-            </label>
-            <input
-              className="appearance-none block w-full bg-white-200 text-gray-700 border shadow rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="form-name"
-              type="text"
-              placeholder="Su nombre y apellido"
-              required
-            />
-          </div>
-          <div className="w-full px-3 mb-6 md:mb-0 inline-block relative">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Cantidad de comensales <span className="text-red-500">*</span>
-            </label>
+            <LabelField label="Cantidad de comensales" required={true} />
             <select
-              id="form-commensals"
-              className="block w-full bg-white border  hover:border-gray-500 px-4 py-2 pr-8 mb-3 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-              required
+              className="block w-full bg-white border hover:border-gray-500 px-4 py-2 pr-8 mb-3 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+              value={commensals}
+              onChange={(e) => setCommensals(e.target.value)}
             >
               <option>1</option>
               <option>2</option>
               <option>3</option>
               <option>4</option>
               <option>5</option>
-              <option>6 o más</option>
+              <option value="6 o más">6 o más</option>
             </select>
           </div>
+
+          {/* Número de Teléfono */}
+          <InputField
+            label="Número de Teléfono"
+            type="tel"
+            placeholder="Su número de teléfono"
+            value={phone}
+            onChange={setPhone}
+            required={true}
+          />
+
+          {/* Descripción */}
           <div className="w-full px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Número de Teléfono
-            </label>
-            <input
+            <LabelField label="Descripción" />
+            <textarea
               className="appearance-none block w-full bg-white-200 text-gray-700 border shadow rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="form-phone"
-              type="tel"
-              placeholder="Su número de teléfono"
-              required
+              placeholder="Detalles adicionales"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
             />
           </div>
 
-          <div className="w-full px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-              Descripción
-            </label>
-            <textarea
-              className="appearance-none block w-full bg-white-200 text-gray-700 border shadow rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-              id="form-description"
-              placeholder="Detalles adicionales"
-            ></textarea>{' '}
-          </div>
+          {/* Botón de enviar */}
           <div className="px-3">
-            <button className="bg-violet-500 hover:bg-violet-700 text-white font-bold mt-4 py-2 rounded rounded-full w-full">
+            <button
+              type="submit"
+              className="bg-violet-500 hover:bg-violet-700 text-white font-bold mt-4 py-2 rounded rounded-full w-full"
+            >
               Anotarse
             </button>
           </div>
         </form>
       </div>
     </div>
+  )
+}
+
+interface InputFieldProps {
+  label: string
+  type: string
+  placeholder: string
+  value: string
+  onChange: (value: string) => void
+  required?: boolean
+}
+function InputField({
+  label,
+  type,
+  placeholder,
+  value,
+  onChange,
+  required = false,
+}: InputFieldProps) {
+  return (
+    <div className="w-full px-3 mb-6 md:mb-0">
+      <LabelField label={label} required={required} />
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="appearance-none block w-full bg-white-200 text-gray-700 border shadow rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        required={required}
+      />
+    </div>
+  )
+}
+
+function LabelField({
+  label,
+  required = false,
+}: {
+  label: string
+  required?: boolean
+}) {
+  return (
+    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+      {label} {required && <span className="text-red-500">*</span>}
+    </label>
   )
 }
