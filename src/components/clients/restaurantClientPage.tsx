4e8d58da7887017c1266604c6644766a574896ae
Paula Domingues <pdomingues@itba.edu.ar>
2024-04-07 22:20:29 -0300

    fix: client page - restaurant

--- src/components/clients/restaurantClientPage.tsx
+++ src/components/clients/restaurantClientPage.tsx
@@ -3,35 +3,38 @@ import CommensalQueueForm from '../forms/commensalQueueForm'
 import PickUpQueueForm from '../forms/pickUpQueueForm'
 import { useParams, useRouter } from 'next/navigation'
 import { Restaurant } from '@/types/restaurant'
-import { getRestaurantBySlug, getRestaurantMenuBySlug } from '@/repositories/restaurant-respository'
+import {
+  getRestaurantBySlug,
+  getRestaurantMenuBySlug,
+} from '@/repositories/restaurant-respository'
 import Loading from '../utils/loading'
+import Gradient from '../restaurants/style/gradient'
 
 export default function RestaurantClientPage() {
   const [showCommensalForm, setShowCommensalForm] = useState(false)
   const [showPickUpForm, setShowPickUpForm] = useState(false)
   const [restaurantData, setRestaurantData] = useState<Restaurant | null>(null)
   const router = useRouter()
-  const restaurantSlug = useParams<{restaurant: string}>()
+  const restaurantSlug = useParams<{ restaurant: string }>()
 
-  const [menuUrl, setMenuUrl] = useState('');
-
+  const [menuUrl, setMenuUrl] = useState('')
 
   const viewMenu = async () => {
-    window.open(await menuUrl, '_blank');
+    window.open(await menuUrl, '_blank')
   }
 
   useEffect(() => {
     const fetchMenuUrl = async () => {
       try {
-        const menuUrl = await getRestaurantMenuBySlug(restaurantSlug.restaurant);
-        setMenuUrl(menuUrl);
+        const menuUrl = await getRestaurantMenuBySlug(restaurantSlug.restaurant)
+        setMenuUrl(menuUrl)
       } catch (error) {
-        console.error('Error setting menu URL:', error);
+        console.error('Error setting menu URL:', error)
       }
-    };
+    }
 
-    fetchMenuUrl();
-  }, []);
+    fetchMenuUrl()
+  }, [])
 
   useEffect(() => {
     const fetchData = async () => {
@@ -43,8 +46,6 @@ export default function RestaurantClientPage() {
         }
         setRestaurantData(restaurant)
       } catch (error) {
-      
-
         console.error('Error fetching restaurant data:', error)
       }
     }
@@ -60,54 +61,58 @@ export default function RestaurantClientPage() {
     setShowPickUpForm(!showPickUpForm) // Toggle the visibility state
   }
 
-  return (
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    <>
-    {restaurantData ? (
-      <>
-      {showCommensalForm && (
-        <CommensalQueueForm toggleCommensalFormVisibility={toggleCommensalFormVisibility} />
-      )}
-      {showPickUpForm && (
-        <PickUpQueueForm togglePickUpFormVisibility={togglePickUpFormVisibility} />
-      )}
-      {!showPickUpForm && !showCommensalForm && (
-        <div className="min-h-screen flex flex-col justify-center relative ">
-          <div className="p-4">
-            <p className="text-7xl mb-2 text-center">
-              <b>{restaurantData?.name}</b>
-            </p>
+  if (!restaurantData) return <Loading />
 
-            <div className="flex flex-col">
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Gradient />
+      <div className="py-5 text-center">
+        <h1 className="text-3xl md:text-4xl font-bold">
+          Bienvenido a {restaurantData?.name}
+        </h1>
+        <p className="mt-2 text-gray-700">
+          Tu experincia culinaria comienza aquí.
+        </p>
+      </div>
+
+      <div className="flex-grow flex flex-col items-center justify-center px-4">
+        {showCommensalForm && (
+          <CommensalQueueForm
+            toggleCommensalFormVisibility={toggleCommensalFormVisibility}
+          />
+        )}
+        {showPickUpForm && (
+          <PickUpQueueForm
+            togglePickUpFormVisibility={togglePickUpFormVisibility}
+          />
+        )}
+        {!showPickUpForm && !showCommensalForm && (
+          <div className="w-full max-w-md">
+            <div className="flex flex-col items-center gap-4">
               <button
                 onClick={toggleCommensalFormVisibility}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-3 py-2 px-4 rounded"
+                className="relative w-full bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 rounded rounded-full"
               >
-                Anotarse para comer
+                Anotarse para Comer
               </button>
               <button
                 onClick={togglePickUpFormVisibility}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-3 py-2 px-4 rounded"
+                className="relative w-full bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 rounded rounded-full"
               >
                 Retirar pedido
               </button>
-
-              { menuUrl && (
-              <button 
-              type="button"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-3 py-2 px-4 rounded"
-              onClick={viewMenu}>
-                Ver menú
-              </button>
+              {menuUrl && (
+                <button
+                  onClick={viewMenu}
+                  className="relative w-full bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 rounded rounded-full"
+                >
+                  Ver Menú
+                </button>
               )}
             </div>
           </div>
-        </div>
-      )}
-      </>
-      ) : 
-      <Loading />
-      }
-    </>
+        )}
+      </div>
+    </div>
   )
 }
--- src/components/navigation/commensal/layout.tsx
+++ src/components/navigation/commensal/layout.tsx
@@ -9,8 +9,8 @@ const Layout: React.FC<LayoutProps> = (props) => {
   return (
     <div className="bg-custom-bg">
       <div className="flex flex-col justify-between min-h-screen">
-        <main className="flex-grow lg:max-w-screen-lg md:max-w-screen-md max-w-screen-sm container mx-auto">
-          <div className="h-full overflow-y-auto">{props.children}</div>
+        <main className="flex-grow lg:max-w-screen-lg md:max-w-screen-md sm:max-w-screen-sm w-full container mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="h-full overflow-y-auto py-4">{props.children}</div>
         </main>
         <Footer />
       </div>
