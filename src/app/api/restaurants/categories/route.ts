4222a746c570082fad946bb4108e328ecd56593b
federojasdr <federico.rojas@devrev.ai>
2024-06-17 12:07:38 -0300

    eslint

--- src/app/api/restaurants/categories/route.ts
+++ src/app/api/restaurants/categories/route.ts
@@ -1,8 +1,8 @@
 import { fetchRestaurantCategories } from '@/services/restaurant-service'
 import { HTTP_RESPONSE_STATUS } from '@/types/https'
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const categories = fetchRestaurantCategories()
 
   return NextResponse.json(
