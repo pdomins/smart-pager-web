1b1183f9aa31caeb043e2c1000ad12fbab7fbc28
Paula Domingues <pdomingues@itba.edu.ar>
2024-07-06 17:46:52 -0300

    feat: dummy upload profile picture

--- src/components/restaurants/sign-up/forms/restaurant-name-form.tsx
+++ src/components/restaurants/sign-up/forms/restaurant-name-form.tsx
@@ -1,4 +1,7 @@
 import { RestaurantFormState } from './restaurant-form'
+import Pfp from 'public/club-mila.png'
+import Image from 'next/image'
+import FileUploadIcon from '@mui/icons-material/FileUpload'
 
 const RestaurantNameForm = ({
   name,
@@ -10,25 +13,49 @@ const RestaurantNameForm = ({
   disabled?: boolean
 }) => {
   return (
-    <label className="block">
-      <span className="text-gray-700">
-        Nombre del restaurante: <span className="text-red-500">*</span>
-      </span>
-      <input
-        type="text"
-        className="form-input mt-1 block w-full px-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:border-violet-700 transition-colors"
-        placeholder="Ingresa el nombre del restaurante"
-        value={name}
-        onChange={(e) =>
-          setFormState((prev) => ({
-            ...prev,
-            name: e.target.value,
-          }))
-        }
-        required
-        disabled={disabled}
-      />
-    </label>
+    <div className="block">
+      <div className="flex items-center">
+        <div className="relative inline-block">
+          <Image
+            src={Pfp.src}
+            alt="Logo"
+            unoptimized={true}
+            width={75}
+            height={75}
+            loading="lazy"
+            className="mr-1 rounded-full cursor-auto z-10"
+          />
+          {!disabled && (
+            <button
+              className="absolute bottom-0 right-0 bg-white/90 rounded-full p-1 shadow z-20"
+              onClick={() => console.log('Edit icon clicked')}
+              type="button"
+            >
+              <FileUploadIcon className="text-sm text-purple-500" />
+            </button>
+          )}
+        </div>
+        <div className="flex-1 pl-4">
+          <span className="text-gray-700">
+            Nombre del restaurante: <span className="text-red-500">*</span>
+          </span>
+          <input
+            type="text"
+            className="form-input mt-1 block w-full px-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:border-violet-700 transition-colors"
+            placeholder="Ingresa el nombre del restaurante"
+            value={name}
+            onChange={(e) =>
+              setFormState((prev) => ({
+                ...prev,
+                name: e.target.value,
+              }))
+            }
+            required
+            disabled={disabled}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 export default RestaurantNameForm
