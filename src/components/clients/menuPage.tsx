cc34b7a6cb48d94e91d1fcc7d9394946a3fb3a61
Paula Domingues <pdomingues@itba.edu.ar>
2024-07-08 10:46:41 -0300

    fix: menu

--- src/components/clients/menuPage.tsx
+++ src/components/clients/menuPage.tsx
@@ -9,14 +9,15 @@ const MenuPage = ({
   menuUrl: string
 }) => {
   return (
-    <div className="min-h-screen flex flex-col justify-center relative">
+    <div className="min-h-screen w-full flex flex-col justify-center relative">
       <div
         className="absolute top-2 left-2 cursor-pointer"
         onClick={toggleMenuPageVisibility}
       >
         <CloseIcon style={{ fontSize: '30px' }} />
       </div>
-      <div className="py-10">
+
+      <div className="w-full max-w-md py-10">
         <ViewMenu menu={menuUrl} />
       </div>
     </div>
