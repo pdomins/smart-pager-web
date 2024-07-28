ac5021a1beed98fec3693805ab3a5a0e57819422
Paula Domingues <pdomingues@itba.edu.ar>
2024-07-28 18:03:52 -0300

    fix: phone format

--- src/lib/phone.ts
+++ src/lib/phone.ts
@@ -2,9 +2,11 @@ export const pattern = /^\+?54\s*11\s*\d{8,10}$/
 export const areaCode = '54'
 
 export const formatPhoneNumber = (phoneNumber: string) => {
-    const countryCode = phoneNumber.slice(0, 2)
-    const areaCode = phoneNumber.slice(2, 4)
-    const localNumber = phoneNumber.slice(4)
-  
-    return `+${countryCode} ${areaCode} ${localNumber}`
-  }
+  const cleanedPhoneNumber = phoneNumber.replace(/\D/g, '')
+
+  const countryCode = cleanedPhoneNumber.slice(0, 2)
+  const areaCode = cleanedPhoneNumber.slice(2, 4)
+  const localNumber = cleanedPhoneNumber.slice(4)
+
+  return `+${countryCode} ${areaCode} ${localNumber}`
+}
